84d9b51c2c2abde8d2caa564f7030e1d6c9540cf
zedong <chenzedong1126@163.com>
2024-11-18 21:38:41 -0500

    update app

--- pages/2_Sector2Models.py
+++ pages/2_Sector2Models.py
@@ -1,5 +1,67 @@
 import streamlit as st
+from pathlib import Path
+import sys
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from stramlit_app_untils import load_sector_anomalies, load_sector_return
 
 st.set_page_config(page_title="2_Sector2Models", page_icon="📈")
 
+sectors = ['Materials', 'Industrials', 'Health Care', 'Real Estate', 'Consumer Discretionary', 'Financials', 
+               'Utilities', 'Information Technology', 'Energy', 'Consumer Staples', 'Communication Services']
+
+
+with st.sidebar:
+    st.header("Input information")
+    default_index = sectors.index('Materials') if 'Materials' in sectors else 0
+    sector = st.selectbox('Sector Name', tuple(sectors), index=default_index)
+
+    start_date = st.date_input('Start Date', value=datetime(2020, 10, 1), min_value=datetime(2019, 9, 26), max_value=datetime(2024, 8, 30))
+    end_date = st.date_input("End Date", value= datetime(2022, 10, 30), min_value=datetime(2019, 9, 26), max_value=datetime(2024, 8, 30))
+
+    if start_date >= end_date:
+        st.error("End date must be after start date")
+
+log_ret = load_sector_return(data_path="Data", sector=sector) * 100
+anomalies = load_sector_anomalies(data_path="Anomalies Dataset", sector=sector)
+
+curr_ret = log_ret.loc[start_date:end_date]
+curr_anomalies = anomalies.loc[start_date:end_date, :]
+
 st.write("placeholder")
+
+fig, ax = plt.subplots(figsize=(20, 15))
+ax.plot(curr_ret.index, curr_ret.values, color='blue',linewidth=0.5)
+offsets = [-0.5, 0, 0.5, -1.0, 1.0, -1.5, 1.5]  # Adjust as needed for more models
+
+for i, model in enumerate(curr_anomalies.columns):
+
+    model_anomaly = curr_anomalies.loc[:, model]
+    model_anomaly_dates = model_anomaly[model_anomaly == 1].index
+    model_anomaly_values =  curr_ret.loc[model_anomaly == 1].values
+
+    offset = offsets[i % len(offsets)]
+    anomaly_display_values = model_anomaly_values + offset
+    ax.scatter(model_anomaly_dates, anomaly_display_values, label=model, s=20)
+    ax.legend()
+st.pyplot(fig)
+
+
+fig, axes = plt.subplots(7, 1, figsize=(12, 20), sharex=True, sharey=True)
+axes = axes.flatten()
+for i, model in enumerate(curr_anomalies.columns):
+    ax = axes[i]    
+    model_anomaly = curr_anomalies.loc[:, model]
+    ax.plot(model_anomaly.index, model_anomaly.values, label=model)
+    ax.set_title(model)
+    ax.set_ylabel("anomaly indicator")
+    ax.set_yticks([0, 1]) 
+    ax.tick_params(axis='x') 
+
+plt.tight_layout()
+plt.subplots_adjust(hspace=0.5)
+st.pyplot(fig)
+
--- stramlit_app_untils.py
+++ stramlit_app_untils.py
@@ -29,28 +29,26 @@ def get_ohlc_dict():
         df_dict[df] = data
     return df_dict
 
-
-def get_sector_dict():
-    sectors = ['Materials', 'Industrials', 'Health Care', 'Real Estate', 'Consumer Discretionary', 'Financials', 
+sectors = ['Materials', 'Industrials', 'Health Care', 'Real Estate', 'Consumer Discretionary', 'Financials', 
                'Utilities', 'Information Technology', 'Energy', 'Consumer Staples', 'Communication Services']
-    
-    path = "Anomalies Dataset"
-    model_list = sorted([x.split('.')[0] for x in next(os.walk(path))[2]])
-    sector_dict = {}
+def load_sector_anomalies(data_path, sector):
+    model_list = sorted([x.split('.')[0] for x in next(os.walk(data_path))[2]])
+    tmp = []
+    for model in model_list:
+        model_data = pd.read_csv(f"{data_path}/{model}.csv", index_col='date')
+        model_data.index = pd.to_datetime(model_data.index)
+        sector_anomalies = (model_data.loc[:, sector]).astype(int).rename(model)
+        tmp.append(sector_anomalies)
+    output = pd.concat(tmp, axis=1)
+    return output
 
-    for sector in sectors:
-        tmp = []
-        for model in model_list:
-            model_data = pd.read_csv(f"{path}/{model}.csv", index_col='date')
-            model_data.index = pd.to_datetime(model_data.index)
+def load_sector_return(data_path, sector):
+    log_return = pd.read_csv(f"{data_path}/log_ret.csv", index_col='date')
+    log_return.index = pd.to_datetime(log_return.index)
+    output = log_return.loc[:, sector]
+    return output
 
-            sector_anomalies = (model_data.loc[:, sector]).astype(int).rename(model)
-            tmp.append(sector_anomalies)
 
-        data = pd.concat(tmp, axis=1)
-        sector_dict[sector] = data
-
-    return sector_dict
 
         
 
